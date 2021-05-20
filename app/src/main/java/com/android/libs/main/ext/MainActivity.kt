2e15b2f58552911adeba241b747e11e5bc1fd514
258474063@qq.com <Quiet800>
2021-05-20 11:01:52 +0800

    Initial commit

---
+++ app/src/main/java/com/android/libs/main/ext/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.android.libs.main.ext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.BaseAdapter
+import androidx.constraintlayout.motion.widget.DesignTool
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
---
+++ commonutils/src/main/java/com/android/libs/ext/A.kt
@@ -0,0 +1,4 @@
+package com.android.libs.ext
+
+class A {
+}
